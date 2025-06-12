8bf2596175149d2d1d1fc0ca8abf8c5c58e06ab4
danielsierraaraujo <daniel.sierra@udla.edu.ec>
2025-06-11 20:58:39 -0500

    Primer Commit - Realize los modelos con sus principales variables

---
+++ SierraDanielAppApuntes/Models/About.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SierraDanielAppApuntes.Models
+{
+    internal class About
+    {
+        public string Name => "Daniel Sierra";
+        public string Description => "Amo el futbol y me gusta mucho la programacion movil y web pero sobre todo el front-end";
+    }
+}
