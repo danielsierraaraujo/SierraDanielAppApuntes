63492a12248620ffa579ced548fe3fce9485066c
danielsierraaraujo <daniel.sierra@udla.edu.ec>
2025-06-11 20:39:07 -0500

    Agregar archivos de proyecto.

---
+++ SierraDanielAppApuntes/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SierraDanielAppApuntes
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SierraDanielAppApuntes/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SierraDanielAppApuntes
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
